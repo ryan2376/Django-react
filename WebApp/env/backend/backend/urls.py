d360a747cc8dc8b2fc912138b891e56dbdbd7c11
ryan <ryannesh2376@gmail.com>
2025-01-30 12:33:58 +0300

    runDjango1

--- WebApp/env/backend/backend/urls.py
+++ WebApp/env/backend/backend/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from api.views import CreateUserView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView #prebuilt views that allow us to obtain our access and refresh tokens
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/user/register/', CreateUserView.as_view(), name="register"),
-    path('')
+    path('api/token/', TokenObtainPairView.as_view(), name="get_token"),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name="refresh"),
+    path('api-auth/', include("rest_framework.urls"),)
 ]
