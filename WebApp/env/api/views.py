68ea925e8926a92243155da3c2b748f998462fad
ryan <ryannesh2376@gmail.com>
2025-01-30 12:12:06 +0300

    migrate api folder

--- WebApp/env/api/views.py
+++ WebApp/env/api/views.py
@@ -7,6 +7,6 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 # Create your views here.
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all() #list of objects
-    serializer_class = User #tells the view the kind of data to expect when creating a new user
+    serializer_class = UserSerializer #tells the view the kind of data to expect when creating a new user
     permission_classes = [AllowAny] #this class specifies who can call this view 
     
